d76ff6ef1eabdb840321cc83e37750864431f42f
natelg1998 <ntlg414@gmail.com>
2021-10-28 14:51:37 -0400

    Updated formatting and requirements

--- test_calculator.py
+++ test_calculator.py
@@ -6,19 +6,14 @@ import calculator
 
 
 class TestCalculator:
-
-
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
-
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
-
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
 
-
     def test_division(self):
-        assert 5 == calculator.division(20,4)
+        assert 5 == calculator.division(20, 4)
